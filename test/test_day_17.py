95559f46b4117d165c150e5863ec60c71ee4d03a
cflynn <cflynn@doximity.com>
2022-12-19 09:52:16 -0800

    add day 17 part 1

---
+++ test/test_day_17.py
@@ -0,0 +1,19 @@
+from advent.calendar.day_17 import part_one, part_two
+
+
+def _input():
+    return """>>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"""
+
+
+def test_part_one():
+    expected = 3068
+    actual = part_one(_input(), 2022)
+
+    assert expected == actual
+
+
+def test_part_two():
+    expected = None
+    actual = part_two(_input(), 2022)
+
+    assert expected == actual
